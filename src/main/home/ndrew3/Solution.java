e82da65fb20238dd0f8612dde0c62975df446647
ndrew219 <Solembum@mail.ru>
2017-06-04 23:56:28 +0300

    тыкс

--- src/main/home/ndrew3/Solution.java
+++ src/main/home/ndrew3/Solution.java
@@ -1,43 +1,39 @@
+package home.ndrew3;
+
 import static java.lang.Math.abs;
 
 /**
  * Created by Андрей on 29.05.2017.
+ * update 04.06.2017
  */
 public class Solution {
-    final private static int ARRAY_IS_EMPTY = -1;
+    final public static int ARRAY_IS_EMPTY = -1;
+    final public static int ARRAY_OVERSIZE_ERROR_CODE = -2;
     final private static int MAX_SIZE_ARRAY = 100000;
-    final private static int ARRAY_OVERSIZE_ERROR_CODE = -2;
-    public static int solution(int[] A){
 
-        int sizeN, averageM, maxDeviation, indexExtreme;
-        maxDeviation = 0;
-        sizeN = A.length;
-        indexExtreme = -1;
-
+    public static int solution(int[] a){
+        int sizeN = a.length;
         if (sizeN > 0 && sizeN <= MAX_SIZE_ARRAY) {
-
-            int sum = 0;
+           int sum = 0;
             for (int i=0; i < sizeN; i++) {
-                sum = sum + A[i];
+                sum = sum + a[i];
             }
-
-            averageM = sum / sizeN;
-
+            int averageM = sum / sizeN;
+            int maxDeviation = 0;
+            int indexExtreme = -1;
             for (int i=0; i < sizeN; i++) {
                 //System.out.print(abs(A[i]-averageM) + " " );
-                if (abs(A[i]-averageM) > maxDeviation) {
-                    maxDeviation = abs(A[i]-averageM);
+                if (abs(a[i]-averageM) > maxDeviation) {
+                    maxDeviation = abs(a[i]-averageM);
                     indexExtreme = i;
                 }
             }
-
-        } else if (sizeN == 0) {
-            return ARRAY_IS_EMPTY;
-            //System.out.println("Количество элементов массива больше 100 000");
+            return indexExtreme;
         } else if (sizeN > MAX_SIZE_ARRAY) {
             return ARRAY_OVERSIZE_ERROR_CODE;
+        } else {
+            return ARRAY_IS_EMPTY;
         }
-        return indexExtreme;
     }
 
     public static void main(String[] args) {
